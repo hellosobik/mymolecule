5f160197900dbf0f5cbe97b33f4c8309df0c445b
hellosobik <hellosobik@gmail.com>
2023-08-18 00:41:39 +0530

    demo navbar

---
+++ mymolecule/app/page.tsx
@@ -0,0 +1,131 @@
+export default function Home() {
+  return (
+    <div className="relative">
+      <div className="font-extrabold text-black text-xl bg-orange-300 sticky top-0">
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>{" "}
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>{" "}
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+      <div>
+        hello world!
+        <br />
+        This is my molecule!
+      </div>
+    </div>
+  );
+}
